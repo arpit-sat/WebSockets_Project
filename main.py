e4d6903806c58b82f16ac28b67c309fe8a7f3754
Arpit Satnalika <60641110+arpit-sat@users.noreply.github.com>
2021-08-09 13:59:57 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import asyncio
+
 import websockets
+from file_read_backwards import FileReadBackwards
+
+with FileReadBackwards("log.txt") as file_read:
+    count = 0
+    lines_list = []
+    while count < 10:
+        curr_line = file_read.readline()
+        if not curr_line:
+            break
+        lines_list.append(curr_line)
+        count = count + 1
 
 
 async def time(websocket, path):
     file = open("log.txt", "r")
+    file.seek(0, 2)
+    log_count = 0
+    lines_list.reverse()
     while True:
-        now = file.readline()
+        if log_count < 10 and log_count < len(lines_list):
+            now = lines_list[log_count]
+        else:
+            now = file.readline()
         if len(now) > 1:
             await websocket.send(now)
         await asyncio.sleep(1)
+        log_count = log_count + 1
 
 
 start_server = websockets.serve(time, "127.0.0.1", 7000)
